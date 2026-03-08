da08874330d2cd71e3aeb481b26a7e2a6f5fc2e4
Malay Dave <malayjdave@gmail.com>
2026-03-08 21:47:48 +0530

    Added argparse

--- raw_log.py
+++ raw_log.py
@@ -1,4 +1,5 @@
 import math
+import argparse
 
 class LogisticRegression:
     
@@ -48,6 +49,11 @@ class LogisticRegression:
         
         
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-x1", type=float, required=True, help="Score for Exam 1")
+    parser.add_argument("-x2", type=float, required=True, help="Score for Exam 2")
+    args = parser.parse_args()
     y_train = [1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1]
     X_train = [[6.4, 4.2], [2.2, 8.4], [8.9, 4.5], [0.3, 5.3], [0.3, 5.2], [5.4, 5.3], [8.1, 4.0], [7.0, 6.0], [9.6, 6.0], [1.0, 9.1], [8.1, 8.4], [9.7, 6.3], [8.3, 7.7], [5.8, 8.2], [2.3, 5.7], [2.3, 4.6], [6.4, 6.2], [2.1, 5.6], [6.5, 7.7], [7.3, 5.0], [9.9, 7.8], [6.8, 9.1], [2.3, 4.2], [2.7, 5.3], [8.8, 5.9], [4.0, 9.5], [2.6, 5.5], [2.6, 7.5], [4.0, 5.3], [5.1, 4.5], [1.1, 7.8], [4.2, 4.4], [10.0, 7.2], [8.6, 4.1], [6.8, 7.2], [6.4, 4.7], [4.5, 9.7], [2.6, 7.0], [9.1, 9.2], [6.4, 7.7], [7.6, 7.2], [5.3, 4.0], [0.2, 9.6], [8.3, 5.8], [8.8, 9.7], [4.9, 4.4], [7.7, 4.8], [5.5, 5.6], [4.2, 5.3], [7.3, 5.2]]
 
@@ -57,9 +63,9 @@ if __name__ == "__main__":
     model.fit(X_train, y_train)
 
 
-    test_student = [[6, 6]]
+    test_student = [[args.x1, args.x2]]
     prediction = model.predict(test_student)
     print(f"Bias: {model.bias}")
     print(f"Weights: {model.weights}")
    
-    print(f"Prediction for [6, 6]: {'Pass' if prediction[0] == 1 else 'Fail'}")
+    print(f"Prediction for [{args.x1}, {args.x2}]: {'Pass' if prediction[0] == 1 else 'Fail'}")
